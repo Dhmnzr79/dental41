735173f3e02c5718ae4ec55273596ea21bf3b337
Denis <denistoday@gmail.com>
2025-12-10 04:24:49 +0200

    Почти готовая главная

--- front-page.php
+++ front-page.php
@@ -11,7 +11,7 @@ get_header();
     <div class="container">
         <div class="hero-mobile__wrapper">
             <div class="hero-mobile__content">
-                <h1 class="hero-mobile__title">Стоматология нового поколения на Камчатке</h1>
+                <h1 class="hero-mobile__title">Стоматология <span class="hero__title-highlight">нового поколения</span> на Камчатке</h1>
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/khan-mob-1.png" alt="Александр Хан" class="hero-mobile__image" loading="lazy">
             </div>
             
@@ -85,7 +85,7 @@ get_header();
         <div class="row">
             <div class="col-sm-12 col-lg-6 hero__left">
                 <h1 class="hero__title">
-                Стоматология нового поколения на Камчатке
+                Стоматология <span class="hero__title-highlight">нового поколения</span> на Камчатке
                 </h1>
         
                 <h2 class="hero__subtitle">
@@ -113,9 +113,6 @@ get_header();
                 <div class="hero__actions">
                     <button type="button" class="btn btn--primary" onclick="openPopup()" aria-label="Записаться на консультацию">Вернуть улыбку</button>
                     <button type="button" class="hero__whatsapp-btn" onclick="window.open('https://wa.me/79084952424', '_blank')" aria-label="Связаться через WhatsApp">
-                        <svg class="hero__whatsapp-icon" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                            <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893A11.821 11.821 0 0020.885 3.488"/>
-                        </svg>
                         <span>Рассчитать стоимость в WhatsApp</span>
                     </button>
             </div>
@@ -146,7 +143,19 @@ get_header();
                     
                     <div class="hero__testimonial" itemscope itemtype="https://schema.org/Review">
                         <p itemprop="reviewBody"><strong itemprop="author" itemscope itemtype="https://schema.org/Person"><span itemprop="name">Лилия Питерская</span>:</strong><br>"Делюсь историей своей новой улыбки"</p>
-                        <a href="<?php echo home_url('/istoriya-moei-ulybki'); ?>" class="hero__testimonial-link" aria-label="Читать отзыв Лилии Питерской">Читать далее</a>
+                        <a href="<?php echo home_url('/istoriya-moei-ulybki'); ?>" class="hero__testimonial-link" aria-label="Читать отзыв Лилии Питерской">
+                            Читать далее
+                            <span class="hero__testimonial-link-icon">
+                                <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                    <mask id="mask0_2916_98" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="0" y="0" width="20" height="18">
+                                        <path d="M20 17.3913V0L0 0V17.3913H20Z" fill="white"/>
+                                    </mask>
+                                    <g mask="url(#mask0_2916_98)">
+                                        <path d="M18.3695 8.69564L11.1956 15.8696M18.3695 8.69564L11.1956 1.52173M18.3695 8.69564H1.63037" stroke="#0B7C87" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </g>
+                                </svg>
+                            </span>
+                        </a>
                     </div>
                 </div>
                 </div>
@@ -548,7 +557,6 @@ get_header();
             <div class="col-sm-12 col-lg-4 plus__right">
                 <div class="plus__card plus__card--featured">
                     <div class="plus__card-content">
-            10.
                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/implant-plus-bg.png" alt="" class="plus__card-featured-bg" aria-hidden="true" loading="lazy">
                         <h3 class="plus__card-title plus__card--featured-title">Имплантация<br>за 1 день</h3>
                         <p class="plus__card-text plus__card--featured-text">Без боли, с временной коронкой сразу. Благодаря нашей цифровой лаборатории вы уходите домой уже с зубом.</p>
@@ -1058,8 +1066,9 @@ get_header();
 
         <div class="row doctors__slider-row">
             <div class="col-sm-12 col-lg-12">
-                <div class="doctors__slider" data-slider="doctors" aria-roledescription="carousel" aria-label="Наши врачи">
-                    <div class="doctors__slider-track">
+                <div class="doctors__slider-wrapper">
+                    <div class="doctors__slider" data-slider="doctors" aria-roledescription="carousel" aria-label="Наши врачи">
+                        <div class="doctors__slider-track">
                         <?php
                         $doctors = new WP_Query(array(
                             'post_type' => 'doctor',
@@ -1155,6 +1164,9 @@ get_header();
                                 </div>
                             </article>
                         <?php endif; ?>
+                        </div>
+
+                        <div class="doctors__pagination" aria-label="Пагинация слайдера врачей"></div>
                     </div>
 
                     <div class="doctors__nav slider-nav">
@@ -1181,8 +1193,6 @@ get_header();
                             </svg>
                         </button>
                     </div>
-
-                    <div class="doctors__pagination" aria-label="Пагинация слайдера врачей"></div>
                 </div>
             </div>
         </div>
--- page-contacts.php
+++ page-contacts.php
@@ -75,3 +75,4 @@ get_header();
 
 
 
+
